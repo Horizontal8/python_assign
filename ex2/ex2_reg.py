28cd3d01b8b6d89b400d0ae12bf94a1753b34195
Horizontal8 <hsli0655@gmail.com>
2017-08-18 17:43:02 -0700

    Update ex2_reg.py

--- ex2/ex2_reg.py
+++ ex2/ex2_reg.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 from scipy.optimize import fmin_bfgs
-from plotData_answer import plotData
+from plotData import plotData
 from mapFeature import mapFeature
-from costFunctionReg_answer import costFunctionReg
+from costFunctionReg import costFunctionReg
 from plotDecisionBoundary import plotDecisionBoundary
-from predict_answer import predict
+from predict import predict
 
 ## Machine Learning Online Class - Exercise 2: Logistic Regression
 #
